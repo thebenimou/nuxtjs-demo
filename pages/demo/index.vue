065abd02794ab3c92ff00bf34652c7d14313482d
thebenimou <benjamin.ejzenberg@gmail.com>
2019-02-22 09:29:25 +0100

    Initial commit

---
+++ pages/demo/index.vue
@@ -0,0 +1,7 @@
+<template>
+	<div>
+		<h1>Welcome !</h1>
+		<h3>So the route is automatically generated, awesome 😀</h3>
+		<p><nuxt-link to="/" class="button--grey">Back home</nuxt-link></p>
+	</div>
+</template>
